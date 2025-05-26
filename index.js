0d8be69e86051418f1c42451dfe3e8e7f071b5ab
mptech2030 <mptechnology2030@gmail.com>
2025-05-26 12:31:18 +0300

    index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.get("/webhook", (req, res) => {
-  const verify_token = "mytoken";
+  const VERIFY_TOKEN = "mytoken"; // تأكد تحط نفس التوكين اللي في واتساب
 
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
   if (mode && token) {
-    if (mode === "subscribe" && token === verify_token) {
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
